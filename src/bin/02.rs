50e61f88ae02bf7ef01b354382cf64ce39891e20
will_marringa <will.marringa@biofiredx.com>
2024-12-02 17:19:04 -0700

    Day 2 Part 2 Done

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -36,7 +36,50 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut total: u32 = 0;
+    for line in input.lines() {
+        let levels: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        if !levels_are_safe(&levels) {
+            for i in 0..levels.len() {
+                let mut mut_levels = levels.clone();
+                mut_levels.remove(i);
+                if levels_are_safe(&mut_levels) {
+                    total += 1;
+                    break;
+                }
+            }
+        } else {
+            total += 1;
+        }
+    }
+
+    Some(total)
+}
+
+fn levels_are_safe(levels: &Vec<i32>) -> bool {
+    let mut is_safe = true;
+    let is_descending = levels[0] - levels[1] > 0;
+    for i in 0..levels.len() - 1 {
+        let diff = levels[i] - levels[i + 1];
+        if is_safe && diff.abs() <= 3 && diff.abs() > 0 {
+            if diff > 0 && !is_descending {
+                is_safe = false;
+                break;
+            }
+            if diff < 0 && is_descending {
+                is_safe = false;
+            }
+        } else {
+            is_safe = false;
+            break;
+        }
+    }
+
+    is_safe
 }
 
 #[cfg(test)]
@@ -52,6 +95,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
